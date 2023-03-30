5d610451201b0bac2b78c25247722e75d3edb365
Nathan Hessler <xmilestegx@gmail.com>
2023-03-30 09:06:56 -0500

    initialize the unlock app

---
+++ unlock/lib/unlock/application.ex
@@ -0,0 +1,20 @@
+defmodule Unlock.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Unlock.Worker.start_link(arg)
+      # {Unlock.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Unlock.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
